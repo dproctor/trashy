e26d3a8fc3de337f072d1339e097c72c1e799271
Devon Proctor <devon.proctor@gmail.com>
2023-06-07 10:59:36 -0700

    Add simple styling for participant checkin flow

--- lib/trashy_web/live/certificate_live.ex
+++ lib/trashy_web/live/certificate_live.ex
@@ -3,49 +3,55 @@ defmodule TrashyWeb.CertificateLive do
 
   def render(assigns) do
     ~H"""
-      <.header class="text-center">
-      Thanks, <%= @participant.first_name %>!
-      <:subtitle>
-      <p><%= @participant.first_name %> <%= @participant.last_name %></p>
-      <p>Valid <%= @formatted_date %></p>
-      <p>All-time cleanups: <%= @total_cleanup_count %></p>
-      <p>Cleanups at this site: <%= @local_cleanup_count %></p>
-      </:subtitle>
-      </.header>
-    <.header class="text-center">
-      Certificate
-      <:subtitle>Claim your rewards</:subtitle>
-    </.header>
-    <table class="table-fixed">
-      <thead>
-        <tr>
-          <th>Merchant</th>
-          <th>Details</th>
-          <th>Claimed</th>
-        </tr>
-      </thead>
-      <tbody>
-        <%= for promotion <- @promotions do %>
-          <tr>
-            <td class="px-1"><%= promotion.promotion.merchant %></td>
-            <td class="px-1"><%= promotion.promotion.details %></td>
-            <td class="px-1">
-              <.input
-                field={promotion.is_claimed}
-                value={promotion.is_claimed}
-                name="promotion"
-                type="checkbox"
-                phx-click="claim_reward"
-                phx-value-promotion_id={promotion.id}
-                disabled={promotion.is_claimed}
-                phx-disconnected={JS.set_attribute({"readonly", true})}
-                phx-connected={JS.remove_attribute("readonly")}
-              />
-            </td>
-          </tr>
-        <% end %>
-      </tbody>
-    </table>
+    <div class="hero min-h-screen bg-base-200">
+      <div class="hero-content flex-col lg:flex-row-reverse">
+        <div class="text-center lg:text-left">
+          <h1 class="text-3xl font-bold">🌁 Thanks, <%= @participant.first_name %>! 🌇</h1>
+          <p><%= @participant.first_name %> <%= @participant.last_name %></p>
+          <p>Valid <%= @formatted_date %></p>
+          <p>All-time cleanups: <%= @total_cleanup_count %></p>
+          <p>Cleanups at this site: <%= @local_cleanup_count %></p>
+        </div>
+        <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <div class="card-body">
+            <div class="text-center lg:text-left">
+              <h2 class="text-xl font-bold">Certificate</h2>
+              <h3 class="text-md">Claim your rewards</h3>
+            </div>
+            <table class="table-fixed">
+              <thead>
+                <tr>
+                  <th>Merchant</th>
+                  <th>Details</th>
+                  <th>Claimed</th>
+                </tr>
+              </thead>
+              <tbody>
+                <%= for promotion <- @promotions do %>
+                  <tr>
+                    <td class="px-1"><%= promotion.promotion.merchant %></td>
+                    <td class="px-1"><%= promotion.promotion.details %></td>
+                    <td class="px-1">
+                      <.input
+                        field={promotion.is_claimed}
+                        value={promotion.is_claimed}
+                        name="promotion"
+                        type="checkbox"
+                        phx-click="claim_reward"
+                        phx-value-promotion_id={promotion.id}
+                        disabled={promotion.is_claimed}
+                        phx-disconnected={JS.set_attribute({"readonly", true})}
+                        phx-connected={JS.remove_attribute("readonly")}
+                      />
+                    </td>
+                  </tr>
+                <% end %>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
     """
   end
 
@@ -56,17 +62,18 @@ defmodule TrashyWeb.CertificateLive do
       true ->
         event = Trashy.Events.get_event!(participant.event_id)
 
-        {:ok, assign(socket,
-        participant_id: participant_id,
-        promotions: Trashy.Promotions.list_event_participant_promotions(participant_id),
-        participant: participant,
+        {:ok,
+         assign(socket,
+           participant_id: participant_id,
+           promotions: Trashy.Promotions.list_event_participant_promotions(participant_id),
+           participant: participant,
+           total_cleanup_count: Trashy.Events.get_total_participant_cleanup_count(participant),
+           local_cleanup_count:
+             Trashy.Events.get_local_participant_cleanup_count(participant, event),
+           formatted_date: Calendar.strftime(event.time, "%m/%d/%Y"),
+           current_user: nil
+         ), layout: false}
 
-        total_cleanup_count: Trashy.Events.get_total_participant_cleanup_count(participant),
-        local_cleanup_count: Trashy.Events.get_local_participant_cleanup_count(participant, event),
-        formatted_date: Calendar.strftime(event.time, "%m/%d/%Y"),
-        current_user: nil
-        )}
-      
       false ->
         raise TrashyWeb.CertificateLive.InvalidCodeError
     end
